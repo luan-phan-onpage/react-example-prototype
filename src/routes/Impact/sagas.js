f3ea8b29cb0586ec0d9d7ae07cb95409d42cc810
Luan Phan <luanphan@onpage.org>
2017-06-28 17:27:48 +0700

    fix sagas

--- src/routes/Impact/sagas.js
+++ src/routes/Impact/sagas.js
@@ -25,7 +25,7 @@ export function* getRepos() {
 }
 
 export function* getGitHubData() {
-    const watcher = yield takeLatest(LOAD_REPOS, getRepos);
+    const watcher = yield* takeLatest(LOAD_REPOS, getRepos);
     yield take(LOCATION_CHANGE);
     yield cancel(watcher);
 }
